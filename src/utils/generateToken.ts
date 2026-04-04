adc8c727d18d3081d61866f1dcc5f35eadffb5c9
OtavioGelain <otavioluizgelain@gmail.com>
2026-04-04 14:14:35 -0300

    fixing erros

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 import { User } from '../entities/User';
 import { env } from '../env/envSchema';
 
-export function generateToken(user: User): string{
-  const secretKey = env.JWT_SECRET
-    const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
-    return token;
+export function generateToken(user: User): string | undefined{
+  const secretKey = env.JWT_SECRET 
+  const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+  return token;
 }
